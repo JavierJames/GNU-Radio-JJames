54d766510bc4fa37a6f348aa4335a9d536ab3799
JavierJames <swifferayubu@gmail.com>
2016-04-28 13:12:37 -0700

    Matlab plots ROC anthenna data
    -Matlab fetches files for anthenna results and plots dataset pdf
    -Matlab fetches files for anthenna results and plots ROC

---
+++ gnuRadio_JavierJames.m
@@ -0,0 +1,84 @@
+close all;
+clc; 
+clear all; 
+
+%read and save H1 data from file, class 1 
+fileID= fopen('H1_dataset.txt','r');
+formatSpec= '%d';
+H1_dataset=fscanf(fileID,formatSpec);
+fclose(fileID);
+
+%read and save H0 data from file, class 2
+fileID= fopen('H0_dataset.txt','r');
+formatSpec= '%d';
+H0_dataset=fscanf(fileID,formatSpec);
+fclose(fileID);
+
+
+%plot the data distribution from both classes
+mu_1= mean(H1_dataset);
+sigma_1=std(H1_dataset);
+
+mu_0= mean(H0_dataset);
+sigma_0=std(H0_dataset);
+
+%display the mean and the standard deviation from both classes 
+display 'mu_1'
+mu_1
+display 'sigma_1'
+sigma_1
+display 'mu_0'
+mu_0
+display 'sigma_0'
+sigma_0
+
+
+
+
+x1= linspace (mu_1-4*sigma_1, mu_1+4*sigma_1)
+x0= linspace (mu_0-4*sigma_0, mu_0+4*sigma_0)
+figure;
+plot(x1,normpdf(x1,mu_1,sigma_1))
+xlabel('dB')
+ylabel('Probability')
+hold on;
+plot(x0,normpdf(x0,mu_0,sigma_0))
+xlabel('dB')
+ylabel('Probability')
+title('Dataset Probability Density Function')
+yL = get(gca, 'YLim'); 
+line([-58 -58],yL,'Color','m','LineStyle','--');
+line([-68.5 -68.5],yL,'Color','g','LineStyle','--');
+hold off ;
+
+legend({'H1 = r(t)+n(t)', 'H0 = n(t)', 'initial threshold = -58 dB', 'optimized threshold = -68.5 dB' }, 'FontSize',12,'FontWeight','bold')
+
+Y1= normpdf(x1,mu_1,sigma_1);
+Y0= normpdf(x1,mu_0,sigma_0);
+
+solve('Y1 ==Y0','x1')
+
+syms x
+Y11=(1/(sigma_1*sqrt(2*pi)))*exp( (- ((x-mu_1)^2)) / (2*pow2(sigma_1) ));
+Y22=(1/(sigma_0*sqrt(2*pi)))*exp( (- ((x-mu_0)^2)) / (2*pow2(sigma_0) ));
+
+Y111=(1/(sigma_1*sqrt(2*pi)))*exp( (- ((x1-mu_1).^2)) / (2*pow2(sigma_1) ));
+Y222=(1/(sigma_0*sqrt(2*pi)))*exp( (- ((x1-mu_0).^2)) / (2*pow2(sigma_0) ));
+
+eqn= Y11==Y22;
+solx = solve(eqn,x)
+
+figure;
+plot(x1,Y111)
+hold on;
+plot(x1,Y222)
+hold off;
+
+%plot the ROC 
+% class_1 = 0.5*randn(100,1); 
+% class_2 = 0.5+0.5*randn(100,1); 
+class_1 = H1_dataset; 
+class_2 = H0_dataset;
+data = [class_1, class_2]; 
+figure;
+ROC_CURVE=roc_curve(data,1,1);
---
+++ roc_curve.m
@@ -0,0 +1,128 @@
+% ----------------------------------------------------------------------- %
+%                           R O C    C U R V E                            %
+% ----------------------------------------------------------------------- %
+% Function "roc_curve" calculates the Receiver Operating Characteristic   %
+% curve, which represents the 1-specificity and sensitivity, of two classes
+% of data, called class_1 and class_2.                                    %
+%                                                                         %
+%   Input parameters                                                      %
+%       · data:     Two-column matrix which stores the data of both classes
+%                   with the following structure:                         % 
+%                     data = [class_1, class_2]                           %
+%                   where class_1 and class_2 are column vectors.         %
+%       · dispp:    (Optional) If dispp is 1, the ROC Curve will be disp- %
+%                   ayed inside the active figure. If dispp is 0, no figure
+%                   will be displayed.                                    %
+%       · dispt:    (Optional) If dispt is 1, the optimum threshold para- %
+%                   meters obtained will be displayed on the MATLAB log.  %
+%                   Otherwise, if dispt is 0, no parameters will be disp- %
+%                   ayed there.                                           %
+%                                                                         %
+%   Output variables                                                      %
+%       · ROC_data: Struct that contains all the curve parameters.        %
+%           - param:    Structu that contains the cuantitative parameters %
+%                       of the obtained curve, which are:                 %
+%               + Threshold:Optimum threshold calculated in order to maxi-%
+%                           mice the sensitivity and specificity values,  %
+%                           which is colocated in the nearest point to    %
+%                           (0,1).                                        %
+%               + AROC:     Area Under ROC Curve.                         %
+%               + Accuracy: Maximum accuracy obtained.                    %
+%               + Sensi:    Optimum threshold sensitivity.                %
+%               + Speci:    Optimum threshold specificity.                %
+%               + PPV:      Positive predicted value.                     %
+%               + NPV:      Negative predicted value.                     %
+%           - curve:    Matrix which contains the specificity and specifi-%
+%                       city of each threshold point in columns.          %
+% ----------------------------------------------------------------------- %
+%   Example of use:                                                       %
+%       class_1 = 0.5*randn(100,1);                                       %
+%       class_2 = 0.5+0.5*randn(100,1);                                   %
+%       data = [class_1, class_2];                                        %
+%       ROC_Curve(data);                                                  %
+% ----------------------------------------------------------------------- %
+%   Author:  Víctor Martínez Cagigal                                      %
+%   Date:    02/09/2015                                                   %
+%   E-mail:  vicmarcag (dot) gmail (dot) com                              %
+% ----------------------------------------------------------------------- %
+function ROC_data = roc_curve(data, dispp, dispt)
+
+    % Setting default parameters and detecting errors
+    if(nargin<2), dispp = 1;    end
+    if(nargin<3), dispt = 1;    end
+    if(size(data,2)~=2), error('Data vector must have only 2 classes.'); end
+    L = size(data,1);                   % Data classes length
+ 
+    % Calculating the threshold values between the data points
+    s_data = unique(sort(data(:)));     % Sorted data points
+    d_data = diff(s_data);              % Difference between consecutive points
+    if(isempty(d_data)), error('Both class data are the same!'); end
+    d_data(length(d_data)+1,1) = d_data(length(d_data));% Last point
+    thres(1,1) = s_data(1) - d_data(1);               % First point
+    thres(2:length(s_data)+1,1) = s_data + d_data./2; % Threshold values
+    
+    % Sorting each class
+    if(mean(data(:,1))>mean(data(:,2))), data = [data(:,2),data(:,1)]; end
+        
+    % Calculating the sensibility and specificity of each threshold
+    curve = zeros(size(thres,1),2);
+    distance = zeros(size(thres,1),1);
+    for id_t = 1:1:length(thres)
+        TP = length(find(data(:,2) >= thres(id_t)));    % True positives
+        FP = length(find(data(:,1) >= thres(id_t)));    % False positives
+        FN = L - TP;                                    % False negatives
+        TN = L - FP;                                    % True negatives
+        
+        curve(id_t,1) = TP/(TP + FN);   % Sensitivity
+        curve(id_t,2) = TN/(TN + FP);	% Specificity
+        
+        % Distance between each point and the optimum point (0,1)
+        distance(id_t)= sqrt((1-curve(id_t,1))^2+(curve(id_t,2)-1)^2);
+    end
+    
+    % Optimum threshold and parameters
+    [~, opt] = min(distance);
+    TP = length(find(data(:,2) >= thres(opt)));   
+    FP = length(find(data(:,1) >= thres(opt)));    
+    FN = L - TP;                                    
+    TN = L - FP;                                    
+        
+    param.Threshold = thres(opt);       % Optimum threshold position
+    param.Sensi = curve(opt,1);         % Optimum threshold's sensitivity
+    param.Speci = curve(opt,2);         % Optimum threshold's specificity
+    param.AROC  = abs(trapz(1-curve(:,2), curve(:,1))); % Area under curve
+    param.Accuracy = (TP+TN)/(TP+TN+FP+FN);             % Maximum accuracy
+    param.PPV   = TP/(TP+FP);           % Positive predictive value
+    param.NPV   = TN/(TN+FN);           % Negative predictive value
+
+    % Plotting if required
+    if(dispp == 1)
+        fill_color = [11/255, 208/255, 217/255];
+        fill([1-curve(:,2); 1], [curve(:,1); 0], fill_color,'FaceAlpha',0.5);
+        hold on; plot(1-curve(:,2), curve(:,1), '-b', 'LineWidth', 2);
+        hold on; plot(1-curve(opt,2), curve(opt,1), 'or', 'MarkerSize', 10);
+        hold on; plot(1-curve(opt,2), curve(opt,1), 'xr', 'MarkerSize', 12);
+        hold off; axis square; grid on; xlabel('1 - specificity'); ylabel('sensibility');
+        title(['AROC = ' num2str(param.AROC)]);
+    end
+    
+    % Log screen parameters if required
+    if(dispt == 1)
+        display(' ------------------------------');
+        display('|     ROC CURVE PARAMETERS     |');
+        display(' ------------------------------');
+        display(['  - Distance:     ' num2str(distance(opt))]);
+        display(['  - Threshold:    ' num2str(param.Threshold)]);
+        display(['  - Sensitivity:  ' num2str(param.Sensi)]);
+        display(['  - Specificity:  ' num2str(param.Speci)]);
+        display(['  - AROC:         ' num2str(param.AROC)]);
+        display(['  - Accuracy:     ' num2str(param.Accuracy*100) '%']);
+        display(['  - PPV:          ' num2str(param.PPV*100) '%']);
+        display(['  - NPV:          ' num2str(param.NPV*100) '%']);
+        display(' ');
+    end
+    
+    % Assinging parameters and curve data
+    ROC_data.param = param;
+    ROC_data.curve = curve;
+end
